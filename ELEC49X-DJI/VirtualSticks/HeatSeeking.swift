4b9035d983238e0fc34ca4c11ba3655c039b3333
Ian <83965677+ikeefe12@users.noreply.github.com>
2023-03-15 17:18:33 -0400

    Update HeatSeeking.swift
    Better formatting

--- ELEC49X-DJI/VirtualSticks/HeatSeeking.swift
+++ ELEC49X-DJI/VirtualSticks/HeatSeeking.swift
@@ -9,49 +9,32 @@ import DJISDK
 
 class HeatSeeking {
     // instance of DroneCommand class that has the capabilities to send commands to the drone
-    var droneCommand: DroneCommand?
+    var droneCommand: DroneCommand
     // Wifi socket from which data is read
     var udpSocket: GCDAsyncUdpSocket?
     // three threads that will run in parallel
     var dataThread: Thread?
     var displayThread: Thread?
     var commandThread: Thread?
+    private let frameWidth = 120
+    private let frameHeight = 84
     // SHARED VARIABLES
-    // Add a DispatchQueue for protecting shared variables
-    private let sharedVarQueue = DispatchQueue(label: "com.example.HeatSeeking.sharedVarQueue", attributes: .concurrent)
-    // Commands to  send to the drone
-    var roll: Double = 0
-    var pitch: Double = 0
-    // This is a 120x80 array of integers representing the thermal image data 
-    var latestFrame: [[Int]]
-    // Flags to tell threads when to update the variables they are using in their loops
-    var newCommands: Bool = false
-    var newFrame: Bool = false
+    private var sharedVars: SharedVars
     
     init() {
-      // this calls the init() function of DroneCommand and gives us an instance of the object from which we can call functions to control DJI drone
+        // SHARED VARIABLE Initialized
+        sharedVars = SharedVars(frameWidth: frameWidth, frameHeight: frameHeight)
+        // this calls the init() function of DroneCommand and gives us an instance of the object from which we can call functions to control DJI drone
         droneCommand = DroneCommand()
-        latestFrame = [[Int]](repeating: [Int](repeating: 0, count: 84), count: 120)
-      // Initialize UDP port 
-    }
-    
-    @objc private func formatData(_ dataBinary: Data) -> [Int] {
-        let hexStr = dataBinary.map { String(format: "%02hhx", $0) }.joined()
-        var numbers = [String]()
-        for i in stride(from: 0, to: hexStr.count, by: 4) {
-            let startIndex = hexStr.index(hexStr.startIndex, offsetBy: i)
-            let endIndex = hexStr.index(startIndex, offsetBy: 4, limitedBy: hexStr.endIndex) ?? hexStr.endIndex
-            numbers.append(String(hexStr[startIndex..<endIndex]))
-        }
-        return numbers.compactMap { Int($0, radix: 16) }
+        // Initialize UDP port 
     }
     
     // This will start streaming data as well as displaying data
-    @objc func enableThermalDataAndDisplay() {
+    @objc func enableThermalDataAndDisplay(view: UIView) {
         dataThread = Thread(target: self, selector: #selector(getData), object: nil)
         dataThread?.start()
         
-        displayThread = Thread(target: self, selector: #selector(dispData), object: nil)
+        displayThread = Thread(target: self, selector: #selector(dispData(view:)), object: nil)
         displayThread?.start()
     }
     
@@ -76,58 +59,58 @@ class HeatSeeking {
         droneCommand?.toggleVirtualSticks(false)
     }
     
-    // dataThread base function
     @objc private func getData() {
         while !Thread.current.isCancelled {
-            // getFrame is a function to be added to this class that will read a frame of data from UDP port
-            let recievedData = getFrame()
-            // Format data using formatData
-            let processedData = formatData(recievedData)
-            // Save data to shared variable latesFrame so it can be used in displayThread
-            setLatestFrame(processedData)
-            setNewFrame(true)
-            // Pass data through tracking algorithm (findTrackingCommands is a function TO BE added to this class)
-            let (trackingRoll, trackingPitch) = findTrackingCommands(processedData)
-            // Save result in shared variables roll and pitch
-            setRollPitch(trackingRoll, trackingPitch)
-            // set newCommands flag to true, indicating to the sendCommand thread to break the loop and update local command variables
-            setNewCommands(true)
+            autoreleasepool {
+                // get processed data (120x84 array of ints)
+                let frame = sharedVars.getFrame()
+                // Save data to shared variable latesFrame so it can be used in displayThread
+                sharedVars.setLatestFrame(frame)
+                sharedVars.setNewFrame(true)
+                // Pass data through tracking algorithm (findTrackingCommands is a function TO BE added to this class, 
+                // which takes a 120x84 array of Ints and returns a tuple (Double, Double) representing the tracking roll and pitch)
+                let (trackingRoll, trackingPitch) = sharedVars.findTrackingCommands(frame)
+                // Save result in shared variables roll and pitch
+                sharedVars.setRollPitch(trackingRoll, trackingPitch)
+                // set newCommands flag to true, indicating to the sendCommand thread to break the loop and update local command variables
+                sharedVars.setNewCommands(true)
+            }
         }
     }
     
     // displayThread base function
-    @objc private func dispData() {
-        // Define the dimensions of the image
-        let width = 120
-        let height = 84
+    @objc private func dispData(view: UIView) {
 
         // Create a UIImageView
-        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: width, height: height))
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: frameWidth, height: frameHeight))
         view.addSubview(imageView)
         
         while !Thread.current.isCancelled {
-            if getNewFrame() {
-                let gray16Image = getLatestFrame()
-                setNewFrame(false)
+            if sharedVars.getNewFrame() {
+                let gray16Image = sharedVars.getLatestFrame()
+                sharedVars.setNewFrame(false)
+                
+                //  release the memory used by the images at the end of each iteration of the loop
+                autoreleasepool {
+                    // Create a CGContext and draw the pixel values to it
+                    let colorSpace = CGColorSpaceCreateDeviceGray()
+                    let bytesPerPixel = MemoryLayout<UInt16>.stride
+                    let bytesPerRow = bytesPerPixel * frameWidth
+                    let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.none.rawValue)
+                    let context = CGContext(data: nil, width: frameWidth, height: frameHeight, bitsPerComponent: 16, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: bitmapInfo.rawValue)!
+                    context.drawGray16Image(gray16Image)
 
-                // Create a CGContext and draw the pixel values to it
-                let colorSpace = CGColorSpaceCreateDeviceGray()
-                let bytesPerPixel = MemoryLayout<UInt16>.stride
-                let bytesPerRow = bytesPerPixel * width
-                let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.none.rawValue)
-                let context = CGContext(data: nil, width: width, height: height, bitsPerComponent: 16, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: bitmapInfo.rawValue)!
-                context.drawGray16Image(gray16Image)
+                    // Create a CGImage from the CGContext
+                    guard let cgImage = context.makeImage() else {
+                        fatalError("Failed to create CGImage.")
+                    }
 
-                // Create a CGImage from the CGContext
-                guard let cgImage = context.makeImage() else {
-                    fatalError("Failed to create CGImage.")
+                    // Create a UIImage from the CGImage
+                    let image = UIImage(cgImage: cgImage)
+
+                    // Update the UIImageView with the new image
+                    imageView.image = image   
                 }
-
-                // Create a UIImage from the CGImage
-                let image = UIImage(cgImage: cgImage)
-
-                // Update the UIImageView with the new image
-                imageView.image = image   
             }
         }
         
@@ -139,13 +122,13 @@ class HeatSeeking {
         var commandRoll = 0.0
         var commandPitch = 0.0
         while !Thread.current.isCancelled {
-            if getNewCommands() {
+            if sharedVars.getNewCommands() {
                // SHARED VARIABLES
-               (commandRoll, commandPitch) = getRollPitch()
+               (commandRoll, commandPitch) = sharedVars.getRollPitch()
                // set newCommands flag to false, allowing the following loop to loop until the other thread sets it back to true
-               setNewCommands(false)
+               sharedVars.setNewCommands(false)
             }
-            while !getNewCommands() {
+            while !sharedVars.getNewCommands() {
                 // Send command
                 droneCommand.sendControlData(0.0, commandPitch, commandRoll, 0.0)
                 // Sleep for 50 milliseconds to achieve 20Hz frequency
@@ -154,6 +137,44 @@ class HeatSeeking {
         }
     }
     
+    // TODO: Fill out function that gets frame and formats the data
+    @objc private func getFrame() -> [[Int]] {
+        var binData = // get frame from port
+        return formatData(binData)
+    }
+    
+    @objc private func formatData(_ dataBinary: Data) -> [[Int]] {
+        let hexStr = dataBinary.map { String(format: "%02hhx", $0) }.joined()
+        var numbers = [String]()
+        for i in stride(from: 0, to: hexStr.count, by: 4) {
+            let startIndex = hexStr.index(hexStr.startIndex, offsetBy: i)
+            let endIndex = hexStr.index(startIndex, offsetBy: 4, limitedBy: hexStr.endIndex) ?? hexStr.endIndex
+            numbers.append(String(hexStr[startIndex..<endIndex]))
+        }
+        let intNumbers = numbers.compactMap { Int($0, radix: 16) }
+        var result = [[Int]](repeating: [Int](repeating: 0, count: frameHeight), count: frameWidth)
+        for i in 0..<frameWidth {
+            for j in 0..<frameHeight {
+                result[i][j] = intNumbers[j * frameWidth + i]
+            }
+        }
+        return result
+    }
+}
+
+struct SharedVars {
+    private let queue = DispatchQueue(label: "com.example.HeatSeeking.sharedVarQueue", attributes: .concurrent)
+    private var _newCommands: Bool = false
+    private var _newFrame: Bool = false
+    private var _latestFrame: [[Int]]
+    private var _roll: Double = 0
+    private var _pitch: Double = 0
+
+    // Initialize the shared variables
+    init(frameWidth: Int, frameHeight: Int) {
+        _latestFrame = [[Int]](repeating: [Int](repeating: 0, count: frameHeight), count: frameWidth)
+    }
+
     // SHARED VARIABLE ACCESS FUNCTIONS
     private func setNewCommands(_ value: Bool) {
         sharedVarQueue.async(flags: .barrier) {
