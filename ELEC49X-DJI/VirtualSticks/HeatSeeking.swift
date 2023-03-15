e2d4836beb58253556e103ff3acb96d39e1cbb17
Ian <83965677+ikeefe12@users.noreply.github.com>
2023-03-15 16:38:50 -0400

    Update HeatSeeking.swift
    Adding thread safety and more formatting.

--- ELEC49X-DJI/VirtualSticks/HeatSeeking.swift
+++ ELEC49X-DJI/VirtualSticks/HeatSeeking.swift
@@ -1,11 +1,9 @@
-// The goal of this class is to create the ability to stream data and communicate with the drone at the same time. 
+// The goal of this class is to create the ability to stream data from local UDP port, display the data, and communicate with the drone at the same time. 
 // Intended use:
-// In CameraFPVViewController.swift when the Start Tracking button is pressed create an instance of this class and then
-// run startTracking() from that instance
-// startTracking() generates the two threads one to stream and process data and the other to take the result of proccessing and 
-// send the correct command to the drone. These threads continue infinitely until stopTracking is called.
-// stopTracking() is called in CameraFPVViewController.swift on the same instance of the HeatSeeking class that was created when the Start 
-// Tracking button was pressed.
+// In CameraFPVViewController.swift create an instance of this class when the view is loaded
+// In the thermalData button handler within CameraFPVViewController.swift, call enableThermalDataAndDisplay or disableThermalDataAndDisplay depending on button state.
+// In the startTracking button handler within CameraFPVViewController.swift, call startTracking
+// In the stopTracking button handler within CameraFPVViewController.swift, call stopTracking
 import UIKit
 import DJISDK
 
@@ -18,20 +16,37 @@ class HeatSeeking {
     var dataThread: Thread?
     var displayThread: Thread?
     var commandThread: Thread?
-    // shared variables that the dataThread writes the results of image processing to so that the commandThread knows what direction to send to drone
-    var vertThrottle: Double = 0
+    // SHARED VARIABLES
+    // Add a DispatchQueue for protecting shared variables
+    private let sharedVarQueue = DispatchQueue(label: "com.example.HeatSeeking.sharedVarQueue", attributes: .concurrent)
+    // Commands to  send to the drone
     var roll: Double = 0
     var pitch: Double = 0
-    var yaw: Double = 0
-    // shared variable flag thay indicates whether commandThread should update it's local variables for direction
+    // This is a 120x80 array of integers representing the thermal image data 
+    var latestFrame: [[Int]]
+    // Flags to tell threads when to update the variables they are using in their loops
     var newCommands: Bool = false
+    var newFrame: Bool = false
     
     init() {
-      // this calls the init() function of DroneCommand and gives us an instance of the object from which we can call functions
+      // this calls the init() function of DroneCommand and gives us an instance of the object from which we can call functions to control DJI drone
         droneCommand = DroneCommand()
-      // TODO: Initialize port 
+        latestFrame = [[Int]](repeating: [Int](repeating: 0, count: 84), count: 120)
+      // Initialize UDP port 
     }
     
+    @objc private func formatData(_ dataBinary: Data) -> [Int] {
+        let hexStr = dataBinary.map { String(format: "%02hhx", $0) }.joined()
+        var numbers = [String]()
+        for i in stride(from: 0, to: hexStr.count, by: 4) {
+            let startIndex = hexStr.index(hexStr.startIndex, offsetBy: i)
+            let endIndex = hexStr.index(startIndex, offsetBy: 4, limitedBy: hexStr.endIndex) ?? hexStr.endIndex
+            numbers.append(String(hexStr[startIndex..<endIndex]))
+        }
+        return numbers.compactMap { Int($0, radix: 16) }
+    }
+    
+    // This will start streaming data as well as displaying data
     @objc func enableThermalDataAndDisplay() {
         dataThread = Thread(target: self, selector: #selector(getData), object: nil)
         dataThread?.start()
@@ -40,11 +55,13 @@ class HeatSeeking {
         displayThread?.start()
     }
     
+    // Stop streaming data
     @objc func disableThermalDataAndDisplay() {
         dataThread?.cancel()
         displayThread?.cancel()
     }
     
+    // start sending commands to the drone, this should only run if the dataThread is running
     @objc func startTracking() {
         // enable virtual sticks on the drone
         droneCommand.toggleVirtualSticks(true)
@@ -52,6 +69,7 @@ class HeatSeeking {
         commandThread?.start()
     }
     
+    // stop tracking
     @objc func stopTracking() {
         commandThread?.cancel()
         // disable virtual stick commmand of the drone
@@ -60,12 +78,20 @@ class HeatSeeking {
     
     // dataThread base function
     @objc private func getData() {
-        while !isCancelled {
-            // TODO: Read Data from port initialized in init()
-            // TODO: Pass data through tracking algorithm (likely a function to be added to this class)
-            // TODO: Save result in shared variable between the two threads (trackingData) - Race Condition ?
+        while !Thread.current.isCancelled {
+            // getFrame is a function to be added to this class that will read a frame of data from UDP port
+            let recievedData = getFrame()
+            // Format data using formatData
+            let processedData = formatData(recievedData)
+            // Save data to shared variable latesFrame so it can be used in displayThread
+            setLatestFrame(processedData)
+            setNewFrame(true)
+            // Pass data through tracking algorithm (findTrackingCommands is a function TO BE added to this class)
+            let (trackingRoll, trackingPitch) = findTrackingCommands(processedData)
+            // Save result in shared variables roll and pitch
+            setRollPitch(trackingRoll, trackingPitch)
             // set newCommands flag to true, indicating to the sendCommand thread to break the loop and update local command variables
-            newCommands = true
+            setNewCommands(true)
         }
     }
     
@@ -79,10 +105,10 @@ class HeatSeeking {
         let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: width, height: height))
         view.addSubview(imageView)
         
-        while !isCancelled {
-            if newFrame {
-                // Get the latest grayscale image data (race condition)
-                let gray16Image = getGray16Image()
+        while !Thread.current.isCancelled {
+            if getNewFrame() {
+                let gray16Image = getLatestFrame()
+                setNewFrame(false)
 
                 // Create a CGContext and draw the pixel values to it
                 let colorSpace = CGColorSpaceCreateDeviceGray()
@@ -110,26 +136,84 @@ class HeatSeeking {
     
     // commandThread base function
     @objc private func sendCommand() {
-        var commandThrottle = 0.0
         var commandRoll = 0.0
         var commandPitch = 0.0
-        var commandYaw = 0.0
-        while !isCancelled {
-            if newCommands {
-               // TODO: Get result of thermal image processing from shared variable (trackingData) - Race Condition ?
-                commandThrottle = vertThrottle
-                commandRoll = roll
-                commandPitch = pitch
-                commandYaw = yaw
+        while !Thread.current.isCancelled {
+            if getNewCommands() {
+               // SHARED VARIABLES
+               (commandRoll, commandPitch) = getRollPitch()
                // set newCommands flag to false, allowing the following loop to loop until the other thread sets it back to true
-               newCommands = false
+               setNewCommands(false)
             }
-            while !newCommands {
-                // TODO: send command based on the newest commands at a rate of 20Hz
-                // Example code - probably wrong 
-                droneCommand.sendControlData(commandThrottle, commandPitch, commandRoll, commandYaw)
+            while !getNewCommands() {
+                // Send command
+                droneCommand.sendControlData(0.0, commandPitch, commandRoll, 0.0)
                 // Sleep for 50 milliseconds to achieve 20Hz frequency
                 Thread.sleep(forTimeInterval: 0.05)
+            }
+        }
+    }
+    
+    // SHARED VARIABLE ACCESS FUNCTIONS
+    private func setNewCommands(_ value: Bool) {
+        sharedVarQueue.async(flags: .barrier) {
+            self.newCommands = value
+        }
+    }
+    
+    private func setNewFrame(_ value: Bool) {
+        sharedVarQueue.async(flags: .barrier) {
+            self.newFrame = value
+        }
+    }
+    
+    private func setLatestFrame(_ value: [[Int]]) {
+        sharedVarQueue.async(flags: .barrier) {
+            self.latestFrame = value
+        }
+    }
+    
+    private func setRollPitch(_ roll: Double, _ pitch: Double) {
+        sharedVarQueue.async(flags: .barrier) {
+            self.roll = roll
+            self.pitch = pitch
+        }
+    }
+    
+    private func getNewCommands() -> Bool {
+        return sharedVarQueue.sync {
+            return self.newCommands
+        }
+    }
+    
+    private func getNewFrame() -> Bool {
+        return sharedVarQueue.sync {
+            return self.newFrame
+        }
+    }
+    
+    private func getLatestFrame() -> [[Int]] {
+        return sharedVarQueue.sync {
+            return self.latestFrame
+        }
+    }
+    
+    private func getRollPitch() -> (Double, Double) {
+        return sharedVarQueue.sync {
+            return (self.roll, self.pitch)
+        }
+    }
+}
+
+extension CGContext {
+    func drawGray16Image(_ gray16Image: [[Int]]) {
+        let width = gray16Image.count
+        let height = gray16Image[0].count
+        for y in 0..<height {
+            for x in 0..<width {
+                let value = UInt16(gray16Image[x][y])
+                let byteOffset = y * width * 2 + x * 2
+                data?.storeBytes(of: value, toByteOffset: byteOffset, as: UInt16.self)
             }
         }
     }
